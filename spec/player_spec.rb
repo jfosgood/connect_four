732b3389a53d47cd55da6cfc2e3c73d84c6273b3
jfosgood <osgood.jake1@gmail.com>
2021-12-23 17:10:01 +0000

    wrote tests for player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -12,3 +12,36 @@
 # -throws error
 
 
+require_relative "spec_helper"
+require "../player.rb"
+
+describe Player do 
+  context "#initialize" do
+    
+    it "raises an error when initialized with invalid hash" do 
+      expect { Player.new({}) }.to raise_error 
+    end 
+    
+    it "does not raise an error when initialized with valid hash" do 
+      input = { color: "X", name: "Someone" }
+      expect { Player.new(input) }.not_to raise_error
+    end 
+  end
+  
+  context "#name" do
+    
+    it "returns the player's name" do
+      input = { color: "X", name: "Someone" }
+      player = Player.new(input)
+      expect(player.name).to eq "Someone"
+    end 
+  end 
+  context "#color" do
+    
+    it "returns the player's color" do 
+      input = { color: "X", name: "Someone" }
+      player = Player.new(input)
+      expect(player.color).to eq "X"
+    end 
+  end 
+end 
