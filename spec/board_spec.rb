f0dea702a07d89b7a5e6ccd7f6acc87d19272b72
jfosgood <osgood.jake1@gmail.com>
2021-12-23 17:54:57 +0000

    Added basic tests for board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,3 +8,80 @@
 # -have #get_winning_poss(x, y)
 #     - uses #get_cell to return list of all combos of 4 that include given coords
 # -have #print_board method that prints visual of cells with values
+
+require_relative "spec_helper"
+require "../board.rb"
+
+describe Board do 
+  context "#initialize" do 
+    
+    it "intitializes board with a grid" do 
+      expect { Board.new({grid: "grid"}) }.not_to raise_error
+    end
+    
+    it "sets the grid to have 6 rows by default" do
+      board = Board.new
+      expect(board.grid).to have(6).things
+    end 
+    
+    it "creates 7 things in each row by default" do 
+      board = Board.new 
+      board.each do | row |
+        expect(row).to have(7).things 
+      end 
+    end 
+  end
+  
+  context "#grid" do 
+    it "returns the grid" do 
+      board = Board.new({grid: "grid"})
+      expect(board.grid).to eq "grid"
+    end 
+  end
+  
+  context "#get_cell" do
+    it "returns cell based on (x, y) coordnate" do 
+      row = ['', '', '', '', '', '', '']
+      grid = []
+      (5).times { grid.push(row) }
+      example_row = ['', '', '', 'Something', '', '', '']
+      grid.push(example_row)
+      board = Board.new(grid: grid)
+      expect(board.get_cell(3, 5)).to eq "Something"
+    end
+  end
+  
+  context "#set_cell" do 
+    it "updates value of cell at (x, y) coordnate" do 
+      Cat = Struct.new(:value)
+      row = ['', '', '', '', '', '', '']
+      grid = []
+      (5).times { grid.push(row) }
+      example_row = [Cat.new("cool"), '', '', '', '', '', '']
+      grid.unshift(example_row)
+      board = Board.new(grid: grid)
+      board.set_cell(0, 0, "meow")
+      expect(board.get_cell(0, 0).value).to eq "meow"
+    end
+  end
+  
+  context "#game_over" do 
+    it "returns :winner if winner? is true" do
+      board = Board.new 
+      board.stub(:winner?) { true }
+      expect(board.game_over).to eq :winner
+    end 
+    it "returns :draw if winner? is false and draw? is true" do
+      board = Board.new 
+      board.stub(:winner?) { false }
+      board.stub(:draw?) { true }
+      expect(board.game_over).to eq :draw
+    end 
+    it "returns false if winner? and draw? are both false" do 
+      board = Board.new 
+      board.stub(:winner?) { false }
+      board.stub(:draw?) { false }
+      expect(board.game_over).to be_false
+    end 
+  end
+end
